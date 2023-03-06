2fb79af82fb83312689f4cf67901e8a24a6b1265
Abdullahi <miraacle64@gmail.com>
2023-03-06 12:58:10 +0100

    feat: setting up the project

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr:false,
+  css: ["@/assets/styles/main.css", "@/assets/styles/styles.scss","@/assets/styles/utils.scss"],
+  components: true,
+  modules: [
+    "@nuxtjs/tailwindcss",
+  ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="text-2xl text-red-800">Hehe it's abdullahi</h2>
+  </div>
+</template>
+
+<script setup></script>
+<style lang="scss">
+@import "../assets/styles/utils.scss";
+
+h2 {
+  // color: $subtle-color;
+  // font-weight: 900;
+}
+</style>

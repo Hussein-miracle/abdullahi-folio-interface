385f79e7036b52a05405f8e2e8db4133d607f98a
Abdullahi <miraacle64@gmail.com>
2023-10-15 07:28:53 +0100

    chore: added hipz-fitz project

--- data/index.js
+++ data/index.js
@@ -1,64 +1,82 @@
-import ProjectItem from "./ProjectItem"
-
-
-
+import ProjectItem from "./ProjectItem";
 
 export const projects = [
-  new ProjectItem("Apace Shopper","This is an e-commerce web app that enables shopper(users) buy items from merchants(storefront)",[
-    'Vuejs',
-    "Nuxtjs",
-    "Typescript",
-    "Tailwindcss"
-    // "Jest"
-  ],
-  "",
-  "https://shopper.useapace.com/create-account"
+  new ProjectItem(
+    "Apace Shopper",
+    "This is an e-commerce web app that enables shopper(users) buy items from merchants(storefront)",
+    [
+      "Vuejs",
+      "Nuxtjs",
+      "Typescript",
+      "Tailwindcss",
+      // "Jest"
+    ],
+    "",
+    "https://shopper.useapace.com/create-account"
   ),
-  new ProjectItem("Dagdag","This is a  fintech web app that provides users with the ability to transfer money to different countries enables using any clients(service provider) listed",[
-    'Vuejs',
-    "Nuxtjs",
-    "Typescript",
-    "Tailwindcss",
-    // "3rd Party Services"
-  ],
-  "",
-  "https://web.dagdag.fun/login/"
+  new ProjectItem(
+    "Dagdag",
+    "This is a  fintech web app that provides users with the ability to transfer money to different countries enables using any clients(service provider) listed",
+    [
+      "Vuejs",
+      "Nuxtjs",
+      "Typescript",
+      "Tailwindcss",
+      // "3rd Party Services"
+    ],
+    "",
+    "https://web.dagdag.fun/login/"
   ),
-  new ProjectItem("Algo Visual","A web app for visualizing algorithm and datastructures.Built by me and Jome-Favourite",[
-    "Nextjs",
-    'Reactjs',
-    "Tailwindcss",
-    "Javascript",
-    // "HTML 5",
-  ],
-  "https://github.com/Hussein-miracle/algo-visual",
-  "https://jf-algo-visual.vercel.app/"
+  new ProjectItem(
+    "Algo Visual",
+    "A web app for visualizing algorithm and datastructures.Built by me and Jome-Favourite",
+    [
+      "Nextjs",
+      "Reactjs",
+      "Tailwindcss",
+      "Javascript",
+      // "HTML 5",
+    ],
+    "https://github.com/Hussein-miracle/algo-visual",
+    "https://jf-algo-visual.vercel.app/"
   ),
-  new ProjectItem("Sneakr ","An e-commerce mobile app for buying dope,cool sneakers for sneakerheads ⚡ like me",[
-    'React Native',
-    "Javascript",
-    "Expo"
-  ],
-  "https://github.com/Hussein-miracle/Vibes-Music-v2",
-  "https://vibes-musics.netlify.app/"
+  new ProjectItem(
+    "Sneakr ",
+    "An e-commerce mobile app for buying dope,cool sneakers for sneakerheads ⚡ like me",
+    ["React Native", "Javascript", "Expo"],
+    "https://github.com/Hussein-miracle/Vibes-Music-v2",
+    "https://vibes-musics.netlify.app/"
   ),
-  new ProjectItem("Vibes Music ","A static music web app with that mimics a mobile device music player",[
-    'Reactjs',
-    "Javascript",
-    "Scss",
-    // "HTML 5",
-  ],
-  "https://github.com/Hussein-miracle/Vibes-Music-v2",
-  "https://vibes-musics.netlify.app/"
+  new ProjectItem(
+    "Vibes Music ",
+    "A static music web app with that mimics a mobile device music player",
+    [
+      "Reactjs",
+      "Javascript",
+      "Scss",
+      // "HTML 5",
+    ],
+    "https://github.com/Hussein-miracle/Vibes-Music",
+    "https://vibes-musics.netlify.app/"
   ),
-  new ProjectItem("Gen NFT App","A simple creative landing page for  a web company  ",[
-    'Reactjs',
-    "Threejs",
-    "Javascript",
-    "Scss",
-    // "HTML 5",
-  ],
-  "https://github.com/Hussein-miracle/nftapp",
-  "https://nft-app-dope.netlify.app/"
-  )
-]
+  new ProjectItem(
+    "HIPz-FITz",
+    "An e-commerce web app for male and female clothings with paystack integrated.",
+    ["Reactjs", "Redux", "Scss", "Paystack"],
+    "https://github.com/Hussein-miracle/hipz-fitz",
+    "https://hipz-fitz.netlify.app/"
+  ),
+  new ProjectItem(
+    "Gen NFT App",
+    "A simple creative landing page for  a web company  ",
+    [
+      "Reactjs",
+      "Threejs",
+      "Javascript",
+      "Scss",
+      // "HTML 5",
+    ],
+    "https://github.com/Hussein-miracle/nftapp",
+    "https://nft-app-dope.netlify.app/"
+  ),
+];
